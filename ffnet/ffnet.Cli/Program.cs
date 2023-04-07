90dd800917b200ca33d58f738e0de60abee3909f
dlion11 <draglion11@gmail.com>
2023-04-07 19:58:34 +0800

    ffnet: Implement app termination

--- ffnet/ffnet.Cli/Program.cs
+++ ffnet/ffnet.Cli/Program.cs
@@ -2,9 +2,35 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private bool _isDone = false;
+        private static readonly string _terminatingInput = "q";
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var app = new Program();
+            await app.RunAsync();
+        }
+
+        public Program()
+        {
+
+        }
+
+        private async Task RunAsync()
+        {
+            while (!_isDone)
+            {
+                // Get input
+                var input = GetInput() ?? "";
+                if (input?.ToLower() == _terminatingInput)
+                    _isDone = true;
+            }
+        }
+
+        private string? GetInput()
+        {
+            Console.Write("Input:\t");
+            return Console.ReadLine();
         }
     }
 }
