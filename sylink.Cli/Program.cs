2d56ce9ba1d4f473e43fd796021a6b0c50528d74
dlion11 <draglion11@gmail.com>
2023-08-11 13:00:20 +0800

    Create root command base

--- sylink.Cli/Program.cs
+++ sylink.Cli/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace sylink.Cli
+﻿using System.CommandLine;
+
+namespace sylink.Cli
 {
     internal class Program
     {
-        static void Main(string[] args)
+        // Commands
+        static RootCommand? _rootCommand;
+
+        static Program()
         {
-            Console.WriteLine("Hello, World!");
+            // Initialize commands, arguments, and options
+            InitializeCommands();
+        }
+
+        static async Task<int> Main(string[] args)
+        {
+            if (_rootCommand is null)
+                return await Task.FromResult(1);
+
+            // Handle root command
+            _rootCommand.SetHandler(() =>
+            {
+
+            });
+
+#if DEBUG
+            //args = new[] { "--version" };
+#endif
+            return await _rootCommand.InvokeAsync(args);
+        }
+
+        static void InitializeCommands()
+        {
+            _rootCommand = new RootCommand("Utility to batch create symbolic links listed from a file.");
         }
     }
 }
